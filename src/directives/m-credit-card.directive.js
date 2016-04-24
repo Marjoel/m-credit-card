e94733219c8dbbd6538296571c00ccdcc03b1d1d
Marjoel <marjoel@marjoel.com>
2016-04-24 13:27:20 -0300

    adding jcb card

--- src/directives/m-credit-card.directive.js
+++ src/directives/m-credit-card.directive.js
@@ -40,12 +40,13 @@
 
 			var brand = '';
 
-			var visa = (/^4[0-9]{12,15}$/),
-				mastercard = (/^5[0-9]{15}$/),
+			var visa = (/^(4)[0-9]{12,15}$/),
+				mastercard = (/^(5)[0-9]{15}$/),
 				amex = (/^(34|37)[0-9]{13}$/),
 				diners = (/^(300|301|302|303|304|305)[0-9]{11}|(36|38|39)[0-9]{12}$/),
 				hipercard = (/^(606282)[0-9]{10}$/),
-				hiper = (/^(637095|637599|637609|637612|637600|637568)[0-9]{10}$/);
+				hiper = (/^(637095|637599|637609|637612|637600|637568)[0-9]{10}$/),
+				jcb = (/^(3088|3096|3112|3158|3337)[0-9]{12}|(35)[0-9]{14}$/);
 
 			if (hipercard.test(value)) {
 				brand = 'hipercard';
@@ -53,23 +54,26 @@
 			else if (hiper.test(value)) {
 				brand = 'hiper';
 			}
-			else if (mastercard.test(value)) {
-				brand = 'mastercard';
+			else if (diners.test(value)) {
+				brand = 'diners';
+			}
+			else if (jcb.test(value)) {
+				brand = 'jcb';
+			}
+			else if (amex.test(value)) {
+				brand = 'amex';
 			}
 			else if (visa.test(value)) {
 				brand = 'visa';
 			}
-			else if (diners.test(value)) {
-				brand = 'diners';
-			}
-			else if (amex.test(value)) {
-				brand = 'amex';
+			else if (mastercard.test(value)) {
+				brand = 'mastercard';
 			}
 			return brand;
 		}
 
 		function cleanCreditCardBrand(element) {
-			element.parent().children().removeClass('visa mastercard hipercard hiper amex');
+			element.parent().children().removeClass('visa mastercard hipercard hiper amex jcb');
 		}
 
 		function addCreditCardBrand(element, value) {
